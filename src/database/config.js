d4432d7c8e0fbe464f4da58d86650922d2a1e47c
viniciussoares18 <vinicius.souza@sptech.school>
2022-05-16 18:39:04 -0300

    API Login e API conectados na Nuvem

--- src/database/config.js
+++ src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    user: "XXXXX",
-    password: "XXXXX",
-    database: "XXXXX",
-    server: "XXXXX",
+    user: "admin-acquatec-9",
+    password: "#Gfgrupo9",
+    database: "acquatec",
+    server: "svr-acquatec-grupo9.database.windows.net",
     pool: {
         max: 10,
         min: 0,
--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -3,49 +3,80 @@
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 
 /* para workbench - local - desenvolvimento */
-CREATE DATABASE FlowTracking;
-USE FlowTracking;
+-- Criação do Banco de Dados
+create database FlowTrackingDataBase;
 
-CREATE TABLE cadEmpresa(
-    idEmpresa CHAR(5) PRIMARY KEY,
-    nomeEmpresa VARCHAR(70),
-    responsavel VARCHAR(70),
-    CNPJ CHAR(18)
+-- Usando o Banco de Dados
+use FLowTrackingDataBase;
+
+
+-- Criando Tabela de Cadastro de Empresa
+create table cadEmpresa (
+idEmpresa char(5) primary key not null,
+nomeEmpresa varchar(70),
+Responsavel varchar(70),
+CNPJ char(18)
 );
 
-CREATE TABLE cadFunc (
-    idcadFunc INT AUTO_INCREMENT,
-    nome VARCHAR(70),
-    email VARCHAR(100),
-    senha VARCHAR(50),
-    fkToken CHAR(5),
-    PRIMARY KEY (idcadFunc, fkToken),
-    FOREIGN KEY (fkToken) REFERENCES cadEmpresa (idEmpresa)
+-- Criação da Tabela de Cadastro de Funcionário
+create table cadFunc (
+idFunc int auto_increment,
+nome varchar(70),
+email varchar(100),
+constraint Email_Verification check (email = '%@%'),
+senha varchar(50),
+fkToken char(5),
+constraint Func_FkToken foreign key (fkToken) references cadEmpresa(idEmpresa),
+primary key (idFunc, fkToken)
 );
 
-CREATE TABLE Estacao(
-    idEstacao INT PRIMARY KEY AUTO_INCREMENT,
-    nomeEstacao VARCHAR(70),
-    linha VARCHAR(70),
-    fkEmpresa CHAR(5),
-    FOREIGN KEY (fkEmpresa) REFERENCES cadEmpresa(idEmpresa)
+-- Criação da Tabela Estação
+create table Estacao (
+idEstacao int primary key auto_increment,
+nomeEstacao varchar(70),
+Linha varchar(70),
+fkEmpresa char(5),
+constraint Estacao_fkEmpresa foreign key (fkEmpresa) references cadEmpresa(idEmpresa)
 );
 
-CREATE TABLE Sensor(
-    idSensor INT PRIMARY KEY AUTO_INCREMENT,
-    localizacao VARCHAR(70),
-    fkEstacao INT,
-    FOREIGN KEY (fkEstacao) REFERENCES Estacao (idEstacao)
+-- Criação da Tabela Anunciante
+create table Anunciante (
+idAnunciante int primary key auto_increment,
+Nome varchar(70),
+Ramo varchar(45)
 );
 
-CREATE TABLE Registro(
-    idRegistro INT AUTO_INCREMENT,
-    dataRegistro DATETIME,
-    registro CHAR(1),
-    fkSensor INT,
-    PRIMARY KEY (idRegistro, fkSensor),
-    FOREIGN KEY (fkSensor) REFERENCES Sensor (idSensor)
+-- Criação da Tabela Anúncio
+create table Anuncio (
+idAnuncio int auto_increment,
+Descrição varchar(50),
+fkEstacao int,
+fkAnunciante int,
+constraint Anuncio_fkEstacao foreign key (fkEstacao) references Estacao(idEstacao),
+constraint Anuncio_fkAnunciante foreign key (fkAnunciante) references Anunciante(idAnunciante),
+primary key (idAnuncio, fkEstacao, fkAnunciante)
 );
+
+-- Criação da Tabela Sensor
+create table Sensor (
+idSensor int primary key auto_increment,
+Localizacao varchar(70),
+fkEstacao int,
+constraint Sensor_fkEstacao foreign key (fkEstacao) references Estacao(idEstacao)
+);
+
+-- Criação da Tabela Registro
+create table Registro (
+idRegistro int,
+Registro char(1),
+constraint Validacao_Registro check (Registro = '1'),
+dtRegistro date,
+fkSensor int,
+constraint Registro_fkSensor foreign key (fkSensor) references Sensor(idSensor),
+primary key (idRegistro,fkSensor)
+);
+
+
 
 
 
