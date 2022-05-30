0378cbe0cad18e6f32038137eb93fe13ca11231b
miguelmendes04 <miguel.mendes@sptech.school>
2022-05-30 11:00:11 -0300

    PROJETO FINALIZADO

---
+++ API/app/connection.js
@@ -0,0 +1,51 @@
+var mysql = require("mysql");
+var sql = require("mssql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "flowtrackingUSER",
+  password: "#Gfgrupo9",
+  database: "flowtrackingapi",
+});
+
+var sqlServerConfig = {
+  server: "svr-acquatec-grupo9.database.windows.net",
+  database: "acquatec",
+  user: "admin-acquatec-9",
+  password: "#Gfgrupo9",
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: true, // for azure
+  },
+};
+function getConnAzure(instrucao) {
+  return new Promise(function (resolve, reject) {
+    sql
+      .connect(sqlServerConfig)
+      .then(function () {
+        return sql.query(instrucao);
+      })
+      .then(function (resultados) {
+        console.log(resultados);
+        resolve(resultados.recordset);
+      })
+      .catch(function (erro) {
+        reject(erro);
+        console.log("ERRO: ", erro);
+      });
+    sql.on("error", function (erro) {
+      return "ERRO NO SQL SERVER (Azure): ", erro;
+    });
+  });
+}
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Conectado ao BD com sucesso!");
+});
+
+module.exports = { connection, getConnAzure };
